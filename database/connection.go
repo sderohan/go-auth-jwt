1d035c538180d67e61db79554949c7f53870ae15
sderohan <rohanshinde7212@gmail.com>
2021-11-03 16:23:07 +0530

    integrate with database connection and post the request data into the datbase

--- database/connection.go
+++ database/connection.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/sderohan/go-auth-jwt/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -16,14 +17,22 @@ const (
 	database_name = "go_auth"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 
 	// Database connection string
 	connectionString := fmt.Sprintf("%s:%s@(%s:%d)/%s?parseTime=true", user_name, password, host, port, database_name)
 
 	// Connect to the database container
-	_, err := gorm.Open(mysql.Open(connectionString))
+	connection, err := gorm.Open(mysql.Open(connectionString))
+
+	// alternatively err variable can be declared first but I preffered this
+	DB = connection
 	if err != nil {
 		log.Fatal("Could not create the database connection ", err.Error())
 	}
+
+	// Migrate the user in database
+	DB.AutoMigrate(&models.User{})
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
 	github.com/gofiber/fiber/v2 v2.21.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211102192858-4dd72447c267 // indirect
 	gorm.io/driver/mysql v1.1.3 // indirect
 	gorm.io/gorm v1.21.12 // indirect
